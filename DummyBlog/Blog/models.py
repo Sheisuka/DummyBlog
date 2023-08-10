8e5a1aae3634e7163c606c0812d09488aeaf25da
sheisuka <xxtornex@gmail.com>
2023-08-10 21:48:07 +0700

    :pencil2: Fix typos

--- DummyBlog/Blog/models.py
+++ DummyBlog/Blog/models.py
@@ -12,14 +12,14 @@ class Post(models.Model):
     slug = models.SlugField(max_length=256)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now())
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
-        indexes = models.Index(fields=['publish'])
+        indexes = [models.Index(fields=['publish'])]
 
     def __str__(self):
         return self.title
