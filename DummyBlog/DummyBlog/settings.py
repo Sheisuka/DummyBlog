b44a6a52f3c22d987a500c38556c0c3d91d1f5ea
sheisuka <xxtornex@gmail.com>
2023-08-22 01:37:16 +0700

    :heavy_plus_sign: Add new app (taggit) to settings

--- DummyBlog/DummyBlog/settings.py
+++ DummyBlog/DummyBlog/settings.py
@@ -21,7 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Blog.apps.BlogConfig'
+    'Blog.apps.BlogConfig',
+    'taggit'
 ]
 
 MIDDLEWARE = [
