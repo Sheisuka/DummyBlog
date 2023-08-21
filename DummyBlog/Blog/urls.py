b3ef49aa6be2b586dfdd0a8c298af6c920096d88
sheisuka <xxtornex@gmail.com>
2023-08-22 03:43:46 +0700

    :sparkles: Add path for searching by tag

--- DummyBlog/Blog/urls.py
+++ DummyBlog/Blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'Blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/share', views.post_share, name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/comment', views.post_comment, name='post_comment')
